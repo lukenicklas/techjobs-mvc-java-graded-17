7126fcb975171d9e6d86338bcaa8d254fc09eb17
Luke Nicklas <lukenicklas18@gmail.com>
2024-02-08 16:23:36 -0600

    Complete

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -1,9 +1,15 @@
 package org.launchcode.techjobsmvc.controllers;
 
+import org.launchcode.techjobsmvc.models.Job;
+import org.launchcode.techjobsmvc.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
 
 import static org.launchcode.techjobsmvc.controllers.ListController.columnChoices;
 
@@ -16,12 +22,26 @@ import static org.launchcode.techjobsmvc.controllers.ListController.columnChoice
 public class SearchController {
 
     @GetMapping(value = "")
-    public String search(Model model) {
-        model.addAttribute("columns", columnChoices);
+    public String search(Model model) { //Model object holds data passed to the view
+        model.addAttribute("columns", columnChoices); //Key value pair
         return "search";
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
 
+    @PostMapping("results") //handles request to results url
+    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+        ArrayList<Job> jobs;//declares arraylist to store the data
+        if (searchTerm.equals("all") || searchTerm.isEmpty()){
+            jobs = JobData.findAll();
+            model.addAttribute("title", "All Jobs"); //adds title to the model
+        } else {
+            jobs = JobData.findByColumnAndValue(searchType,searchTerm);
+        }
+        model.addAttribute("columns", columnChoices); //adds column choices to the model
+        model.addAttribute("jobs", jobs); //adds jobs to the model
+
+        return "search";
+    }
 }
 
